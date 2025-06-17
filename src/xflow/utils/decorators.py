be7454df751f08877ae7fd7de9b397564847479f
Andrew-Xu <qiyuanxu95@gmail.com>
2025-06-17 09:45:04 +0200

    update decorator progress wrapper

--- src/xflow/utils/decorators.py
+++ src/xflow/utils/decorators.py
@@ -2,17 +2,17 @@ from functools import wraps
 from typing import Callable
 from tqdm import tqdm
 
-def with_progress(desc: str = "Processing", unit: str = "item"):
+def with_progress(func: Callable) -> Callable:
     """Decorator to add progress bar to functions that process iterables."""
-    def decorator(func: Callable) -> Callable:
-        @wraps(func)
-        def wrapper(*args, **kwargs):
-            # Find the iterable argument (usually first positional arg)
-            if args and hasattr(args[0], '__iter__'):
-                iterable = args[0]
-                # Wrap with tqdm
-                progress_iterable = tqdm(iterable, desc=desc, unit=unit)
-                return func(progress_iterable, *args[1:], **kwargs)
-            return func(*args, **kwargs)
-        return wrapper
-    return decorator
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        # Find the iterable argument (usually first positional arg)
+        if args and hasattr(args[0], '__iter__'):
+            iterable = args[0]
+            # Wrap with tqdm
+            progress_iterable = tqdm(iterable)
+            # Replace first argument with progress-wrapped version
+            new_args = (progress_iterable,) + args[1:]
+            return func(*new_args, **kwargs)
+        return func(*args, **kwargs)
+    return wrapper
